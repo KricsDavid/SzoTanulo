5c27b84a13c3ff7565e27c7325386d9fa4c5c2ec
KricsDavid <david@kricsfalusi.hu>
2024-11-28 12:52:40 +0100

    Add project files.

---
+++ SzoTanulo/Word.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzoTanulo
+{
+    internal class Word
+    {
+        public int Id { get; set; }
+        public string WordText { get; set; }
+        public string Meaning { get; set; }
+        public int Learned { get; set; }
+    }
+}
